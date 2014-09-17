58f4e505af0ba59b25917368eb96bf5c31f1b1d0
Kanishka Ganguly <kanishkaganguly2002@gmail.com>
2014-09-17 07:23:27 +0530

    Uploading Commented Code

--- CurlROS/curlROS.cpp
+++ CurlROS/curlROS.cpp
@@ -1,22 +1,27 @@
 #include <stdio.h>
 #include <curl/curl.h>
 #include <ros/ros.h>
+#include <string.h>
+
+using namespace std;
 
 int main(int argc, char** argv){
+    //Initialize cURL variables
   CURL *curl;
   CURLcode res;
-  
+
+    //INIT ROS
   ros::init(argc, argv, "curlROS_node");
   ros::start();
+
+    //INIT cURL
   curl_global_init(CURL_GLOBAL_ALL);
   curl = curl_easy_init();
   ROS_INFO_STREAM("Starting cURL");
+
   if(curl) {
-   CURL *curl;
-   CURLcode res;
-   curl = curl_easy_init();
-   if(curl) {
     curl_easy_setopt(curl, CURLOPT_URL, "http://192.168.0.103/?data=FAN");
+      //curl_easy_setopt(curl, CURLOPT_URL, "http://192.168.0.103/?data=LIGHT");
     res = curl_easy_perform(curl);
     curl_easy_cleanup(curl);
   }
@@ -24,5 +29,4 @@ int main(int argc, char** argv){
   ros::spin();
   ros::shutdown();
   return 0;
-}
 }
